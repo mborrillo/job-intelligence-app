899fcd4beaec18c831017993c7eec865436b4876
Marcos  Borrillo <68borrismar@gmail.com>
2026-04-22 13:31:45 +0200

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,11 +1,18 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///./jobs.db"
+DATABASE_URL = os.getenv("postgresql://neondb_owner:npg_z2p3fUHDAMLE@ep-super-frost-abq1wnsj-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
